e05ad1729bfb8db1769db7148ca14dd7fb791ec9
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2020-08-21 13:47:02 +0200

    folder structure

---
+++ Nedeljni_3_Marko_Gacinovic_Jasmina_Kostadinovic/ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Nedeljni_3_Marko_Gacinovic_Jasmina_Kostadinovic.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
